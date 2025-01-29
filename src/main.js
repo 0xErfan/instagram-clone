141ef95ab26dc048218e52f5685adce144c15d34
0xerfan <eftekharierfan5555555555@gmail.com>
2025-01-29 16:44:15 +0330

    update main file extension

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import App from '@/App.vue'
 import './assets/css/tailwind/output.css'
 
-import { createApp } from 'vue'
+import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3.2.37/dist/vue.esm-browser.js'
 
 createApp(App).mount('#app')
