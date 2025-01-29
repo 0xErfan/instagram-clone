6ce1ffcbbad923d5af3818494e88535fcc96d021
0xerfan <eftekharierfan5555555555@gmail.com>
2025-01-29 17:11:43 +0330

    update base address

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'vue-newbie',
+  base: '/vue-newbie/',
   plugins: [
     vue(),
     vueJsx(),
