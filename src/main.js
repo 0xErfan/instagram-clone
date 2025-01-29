392d424877d4e357404e094f016333e988428188
0xerfan <eftekharierfan5555555555@gmail.com>
2025-01-29 16:51:59 +0330

    update build file

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import App from '@/App.vue'
-import './assets/css/tailwind/output.css'
-
-import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3.2.37/dist/vue.esm-browser.js'
-
-createApp(App).mount('#app')
