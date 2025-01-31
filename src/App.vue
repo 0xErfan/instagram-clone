91cde57d6c35e151960b906ddb383d1b86539d54
0xerfan <eftekharierfan5555555555@gmail.com>
2025-01-31 21:49:56 +0330

    add lifecycle method components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 
-import Counter from "./components/tepmplates/Counter.vue"
-import TemperatureColor from "./components/tepmplates/TemperatureColor.vue"
-import CountDown from "./components/tepmplates/CountDown.vue"
-import LoginForm from "./components/tepmplates/LoginForm.vue"
-import QuizApp from "./components/tepmplates/QuizApp.vue"
-import BookApp from "./components/tepmplates/BookApp.vue";
-import TodoApp from "./components/tepmplates/TodoApp.vue";
-import ECommerce from "./components/tepmplates/ECommerce.vue"
-import QuestionToggles from "./components/tepmplates/QuestionToggles.vue"
-import RestaurantMenu from "./components/tepmplates/RestaurantMenu.vue"
-import LearnVue_is from "./components/tepmplates/LazyLoading.vue"
+import Counter from "./components/templates/Counter.vue"
+import TemperatureColor from "./components/templates/TemperatureColor.vue"
+import CountDown from "./components/templates/CountDown.vue"
+import LoginForm from "./components/templates/LoginForm.vue"
+import QuizApp from "./components/templates/QuizApp.vue"
+import BookApp from "./components/templates/BookApp.vue";
+import TodoApp from "./components/templates/TodoApp.vue";
+import ECommerce from "./components/templates/ECommerce.vue"
+import QuestionToggles from "./components/templates/QuestionToggles.vue"
+import RestaurantMenu from "./components/templates/RestaurantMenu.vue"
+import LearnVue_is from "./components/templates/LazyLoading.vue"
+import LifeCycles from "./components/templates/LifeCycles.vue"
 
 </script>
 
@@ -27,5 +28,6 @@ import LearnVue_is from "./components/tepmplates/LazyLoading.vue"
     <QuestionToggles />
     <RestaurantMenu />
     <LearnVue_is />
+    <LifeCycles />
   </main>
 </template>
---
+++ src/components/templates/LifeCycles.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onBeforeUnmount, onMounted, onUpdated, ref } from 'vue';
+
+const counter = ref(0)
+
+onMounted(() => {
+  console.log('Component mounted i think')
+})
+
+onUpdated(() => {
+  console.log('component updated.')
+})
+
+onBeforeUnmount(() => {
+  alert('ops')
+  console.log('unbeforeunmount lifecycle runs here i think hahahaa.')
+  localStorage.setItem('beforeunmount', 'done')
+})
+
+</script>
+
+<template>
+  <section class="flex items-center justify-center py-28 w-full bg-gray-400">
+    <div @click="counter++">Vue LifeCycle Methods</div>
+  </section>
+</template>
