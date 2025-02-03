56693fa88b1af4d329b053e895186ba8f106b8f3
0xerfan <eftekharierfan5555555555@gmail.com>
2025-02-03 23:06:20 +0330

    something i think

--- src/components/BottonComponent.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import { ThemContext } from "../App"
-const BottonComponent = () => {
-    const themVal = useContext(ThemContext)
-    return (
-        <div>BottonComponent {'+ ' + themVal}</div>
-    )
-}
-export default BottonComponent
