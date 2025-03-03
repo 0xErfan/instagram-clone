ef03fdfc21d2610397e8caaaae2659c23d2418aa
0xerfan <eftekharierfan5555555555@gmail.com>
2025-03-03 21:46:50 +0330

    refactor user controller response formats

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -8,19 +8,19 @@ const banUser = async (req: IncomingMessage, res: ServerResponse) => {
     try {
 
         const _id = extractValueFromSegment(req.url, 3)
-        if (!_id) return sendResponse(res, 403, { message: "No _id provided" });
+        if (!_id) return sendResponse(res, 403, { errors: ["No _id provided"], success: false });
 
         const userData = await UserModel.findOne({ _id })
 
         if (userData) {
             userData.isBan = true
             await userData.save()
-            sendResponse(res, 200, { message: 'User banned successfully.' })
-        } else sendResponse(res, 404, { message: 'User not found.' })
+            sendResponse(res, 200, { message: 'User banned successfully.', success: true })
+        } else sendResponse(res, 404, { errors: ['User not found.'], success: false })
 
     } catch (error) {
         console.log(error)
-        sendResponse(res, 500, error)
+        sendResponse(res, 500, { errors: [error], success: false })
     }
 }
 
@@ -29,46 +29,52 @@ const unBanUser = async (req: IncomingMessage, res: ServerResponse) => {
     try {
 
         const _id = extractValueFromSegment(req.url, 3)
-        if (!_id) return sendResponse(res, 403, { message: "No _id provided" });
+        if (!_id) return sendResponse(res, 403, { errors: ["No _id provided"], success: false });
 
         const userData = await UserModel.findOne({ _id })
 
         if (userData) {
             userData.isBan = false
             await userData.save()
-            sendResponse(res, 200, { message: 'User unbanned successfully.' })
+            sendResponse(res, 200, { message: 'User unbanned successfully.', success: true })
         } else sendResponse(res, 404, { message: 'User not found.' })
 
     } catch (error) {
         console.log(error)
-        sendResponse(res, 500, error)
+        sendResponse(res, 500, { errors: [error], success: false })
     }
 }
 
 const deleteUser = async (req: IncomingMessage, res: ServerResponse) => {
 
-    if (req.method !== 'DELETE') return sendResponse(res, 403, { message: "Method not allowed" })
+    if (req.method !== 'DELETE') {
+        sendResponse(res, 403, { errors: ["Method not allowed"], success: false })
+        return;
+    }
 
     try {
 
         const _id = extractValueFromSegment(req.url, 3)
-        if (!_id) return sendResponse(res, 403, { message: "No _id provided" });
+        if (!_id) return sendResponse(res, 403, { errors: ["No _id provided"], success: false });
 
         const deleteUser = await UserModel.findOneAndDelete({ _id })
 
         if (deleteUser) {
-            sendResponse(res, 200, { message: `User account deleted successfully` })
-        } else sendResponse(res, 403, { message: "User not found" })
+            sendResponse(res, 200, { message: `User account deleted successfully`, success: true })
+        } else sendResponse(res, 403, { errors: ["User not found"], success: false })
 
     } catch (error) {
         console.log(`deleteUser Error: ${error}`);
-        sendResponse(res, 500, { message: error!.toString() })
+        sendResponse(res, 500, { errors: [error!.toString()], success: false })
     }
 }
 
 const updateUser = async (req: IncomingMessage, res: ServerResponse) => {
 
-    if (req.method !== 'PUT') return sendResponse(res, 403, { message: "Method not allowed." })
+    if (req.method !== 'PUT') {
+        sendResponse(res, 403, { errors: ["Method not allowed."], success: false })
+        return;
+    }
 
     try {
 
@@ -76,11 +82,11 @@ const updateUser = async (req: IncomingMessage, res: ServerResponse) => {
         const userData = await getReqBody(req)
 
         const updatedUserData = await UserModel.findOneAndUpdate({ _id }, { ...userData })
-        sendResponse(res, 200, { message: 'User data updated successfully.', user: updatedUserData })
+        sendResponse(res, 200, { message: 'User data updated successfully.', data: { user: updatedUserData }, success: true })
 
     } catch (error) {
         console.log(`Internal error: ${error}`)
-        sendResponse(res, 500, { message: `Internal Server Error: ${error}` })
+        sendResponse(res, 500, { errors: [`Internal Server Error: ${error}`], success: false })
     }
 }
 
